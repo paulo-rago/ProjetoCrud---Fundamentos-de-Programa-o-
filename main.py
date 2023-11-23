9892698836802a65fa5aaadcaa7ac10fca88ba50
paulo-rago <149490954+paulo-rago@users.noreply.github.com>
2023-11-22 22:37:46 -0300

    Quarto commit Paulo Ricardo

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ def limpar_tela():
 
 # Função para adicionar um livro à biblioteca
 def adicionar_livro(biblioteca):
+    print("Adicionar Livro")
+    dados_livro = input("Digite Nome, Autor, Categoria, Custo (separados por vírgula): ").split(",")
+    nome, autor, categoria, custo = dados_livro
+    custo = float(custo)
+    livro = {"Nome": nome, "Autor": autor, "Categoria": categoria, "Custo": custo}
+    biblioteca.append(livro)
+    print("Livro adicionado à biblioteca.")
 
 # Função para listar todos os livros na biblioteca
 def listar_livros(biblioteca):
@@ -19,6 +26,35 @@ def excluir_livro(biblioteca, livro_id):
 
 # Função para calcular o total gasto na coleção de livros
 def calcular_gastos_totais(biblioteca):
+
+import os
+import time
+
+def livro_aleatorio():
+    try:
+        with open("livros.txt", mode="r", encoding="utf-8") as arquivo:
+            livros = arquivo.readlines()
+
+            indice = (len(livros) * int(time.time())) % len(livros)
+
+            livro_aleat = livros[indice].strip()  # Obtém o livro aleatório
+
+            # Separação dos detalhes do livro
+            detalhes = livro_aleat.split(",")  # Suponha que o arquivo "livros.txt" esteja no formato: "Título, Autor, Gênero, Custo"
+            titulo = detalhes[0]
+            autor = detalhes[1]
+            genero = detalhes[2]
+            preco = detalhes[3]
+
+            # Adiciona os detalhes à sessão "Livros Indicados" no arquivo "biblioteca.csv"
+            with open("biblioteca.csv", mode="a", newline="", encoding="utf-8") as arquivo_csv:
+                campos = ["Nome", "Autor", "Categoria", "Custo"]
+                escritor = csv.DictWriter(arquivo_csv, fieldnames=campos)
+                escritor.writerow({"Nome": titulo, "Autor": autor, "Categoria": genero, "Custo": preco})
+
+            print("O livro indicado foi adicionado à biblioteca.")
+    except FileNotFoundError:
+        print("Arquivo 'livros.txt' não encontrado.")
 
 biblioteca = []
 
@@ -36,7 +72,8 @@ while True:
     print("3. Atualizar Livro")
     print("4. Excluir Livro")
     print("5. Calcular Gastos Totais")
-    print("6. Sair")
+    print("6. Indique um Livro Aleatório")
+    print("7. Sair")
 
     # Solicita a escolha do usuário
     opcao = input("Escolha uma opção: ")
@@ -58,6 +95,8 @@ while True:
         total_gastos = calcular_gastos_totais(biblioteca)
         print(f"Total gasto em livros: R$ {total_gastos:.2f}")
     elif opcao == "6":
+        livro_aleatorio()
+    elif opcao == "7":
         # Salva os dados da biblioteca no arquivo CSV antes de sair
         with open("biblioteca.csv", mode="a", newline="") as arquivo:
             campos = ["Nome", "Autor", "Categoria", "Custo"]
